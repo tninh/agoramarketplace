c617dadebe0348d1587a0b1760b674423f5e78b8
tninh <tri.ninh01@gmail.com>
2017-05-06 17:15:56 -0700

    Add star rating

---
+++ yeumai/starrating/index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<link rel="icon" type="image/png" href="../assets/img/logo/4You_Logo.png">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+	<title>star</title>
+
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+	<!--     Fonts and icons     -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<!-- CSS Files -->
+    <!-- <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../assets/css/material-kit.css" rel="stylesheet"/>
+		<link href="../assets/css/mystyle.css" rel="stylesheet" /> -->
+		<!-- <link href="../assets/css/star-rating.min.css" rel="stylesheet" /> -->
+		<!-- <link rel="stylesheet" href="../assets/themes/krajee-fa/theme.css" media="all" type="text/css"/>
+    <link rel="stylesheet" href="../assets/themes/krajee-svg/theme.css" media="all" type="text/css"/>
+    <link rel="stylesheet" href="../assets/themes/krajee-uni/theme.css" media="all" type="text/css"/> -->
+</head>
+<body>
+  <div class="container">
+    <div class="row lead">
+          <div id="stars" class="starrr"></div>
+          You gave a rating of <span id="count">0</span> star(s)
+  	</div>
+  </div>
+</body>
+<!-- <script src="../assets/js/jquery.min.js" type="text/javascript"></script> -->
+	<!-- <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="../assets/js/material.min.js"></script> -->
+	<!-- <script src="../assets/js/jquery.barrating.min.js"></script> -->
+	<!-- <script type="text/javascript">
+   $(function() {
+      $('#example').barrating({
+        theme: 'fontawesome-stars'
+      });
+   });
+</script> -->
+	<!-- <script src="../assets/js/starrating.js"></script>
+	<script src="../assets/js/star-rating.min.js"></script> -->
+	<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+	<!-- <script src="../assets/js/nouislider.min.js" type="text/javascript"></script> -->
+
+	<!--  Plugin for the Datepicker, full documentation here: http://www.eyecon.ro/bootstrap-datepicker/ -->
+	<!-- <script src="../assets/js/bootstrap-datepicker.js" type="text/javascript"></script>
+	<script src="../assets/js/starrating.js"></script> -->
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<script src="starrating.js"></script>
+
+</html>
