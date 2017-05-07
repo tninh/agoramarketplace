235d39d10170c638e813f04e7ceb1fb0f8b85de1
phongdong <phongdong@gmail.com>
2017-05-07 10:29:57 -0700

    rating.sql
    change comments data type from blob to varchar

--- Rating.sql
+++ Rating.sql
@@ -26,24 +26,45 @@ SET time_zone = "+00:00";
 -- Table structure for table `Rating`
 --
 
+
 CREATE TABLE `Rating` (
   `userEmail` varchar(50) NOT NULL,
   `productId` int(8) NOT NULL,
   `rating` int(8) NOT NULL,
-  `commnents` blob NOT NULL
+  `comments` varchar(1024) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Rating`
 --
 
-INSERT INTO `Rating` (`userEmail`, `productId`, `rating`, `commnents`) VALUES
-('admin@group-all.com', 1, 4, 0x676f6f64),
-('admin@group-all.com', 2, 5, 0x76657279206e636965),
-('admin@group-all.com', 3, 3, 0x6e696365),
-('coolprofsinn@group-all.com', 1, 4, 0x6e69636520636172),
-('coolprofsinn@group-all.com', 2, 3, 0x6e69636520636f6c6f72),
-('ducnguyen@group-all.com', 1, 4, 0x69206c696b65206974);
+INSERT INTO `Rating` (`userEmail`, `productId`, `rating`, `comments`) VALUES
+('admin@group-all.com', 1, 1, 'bad'),
+('admin@group-all.com', 2, 5, 'good'),
+('admin@group-all.com', 3, 3, 'nice one'),
+('coolprofsinn@group-all.com', 1, 4, 'i like it'),
+('coolprofsinn@group-all.com', 2, 3, 'not so bad'),
+('ducnguyen@group-all.com', 1, 4, 'hmm'),
+('admin@group-all.com', 3, 3, 'amzing'),
+('admin@group-all.com', 3, 3, 'average'),
+('admin@group-all.com', 3, 3, 'like it'),
+('admin@group-all.com', 3, 3, 'alright'),
+('admin@group-all.com', 3, 3, 'cool'),
+('admin@group-all.com', 3, 3, 'recommended'),
+('admin@group-all.com', 3, 3, 'how so'),
+('admin@group-all.com', 3, 3, 'nice color'),
+('Anonymous', 1, 4, 'strong engine'),
+('Anonymous', 1, 4, 'cant wait to get it'),
+('Anonymous', 1, 4, 'the beast'),
+('Anonymous', 1, 4, 'holo'),
+('Anonymous', 1, 4, 'nice'),
+('Anonymous', 1, 4, 'very nice'),
+('Anonymous', 1, 4, 'amzing'),
+('admin@group-all.com', 1, 1, 'ahhh'),
+('Anonymous', 1, 4, 'beat it'),
+('Anonymous', 2, 5, 'damn'),
+('abc@email.com', 2, 4, 'wow');
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
