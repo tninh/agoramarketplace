9bdd4f3e6bd0fbe89de9b06c4f9b28f61dec85f1
providen1990 <trunghuynhb@gmail.com>
2017-05-08 12:47:33 -0700

    Last5Visited

--- yeumai/lfive.php
+++ yeumai/lfive.php
@@ -8,122 +8,179 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" href="assets/img/logo/agora.png">
 
     <title>Products</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/shop-homepage.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/css/shop-homepage.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    
-    <!-- Connect to database -->
-
 </head>
 
 <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Agora Marketplace</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="index.php">Home</a>
-                    </li>
-                    <li>
-                        <a href="about.php">About</a>
-                    </li>
-                    <li>
-                        <a href="products.php">Products</a>
-                    </li>
-                    <li>
-                        <a href="news.php">News</a>
-                    </li>
-                    <li>
-                        <a href="contacts.php">Contacts</a>
-                    </li>
-                    <li>
-                        <a href="users/users_main.php">Users</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
-    <!-- Page Content -->
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!--<img class="pull-left" src="assets/img/logo/agora.png">-->
+            <a class="navbar-brand" href="#">Agora Marketplace</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="main.php">Home</a>
+                </li>
+                <li>
+                    <a href="about.php">About</a>
+                </li>
+                <li>
+                    <a href="products.php">Products</a>
+                </li>
+                <li>
+                    <a href="news.php">News</a>
+                </li>
+                <li>
+                    <a href="contacts.php">Contacts</a>
+                </li>
+                <li>
+                    <a href="users/users_main.php">Users</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
 
-        <div class="row">
+<!-- Page Content -->
+<div class="container">
 
-            <div class="col-md-3">
-                <p class="lead">Agora Marketplace</p>
-                <div class="list-group">
-                    <a href="products/lfive.php" class="list-group-item">Last five visited items</a>
-                    <a href="mfive.php" class="list-group-item">Five most visited items</a>
-                </div>
+    <div class="row">
+
+        <div class="col-md-3">
+            <p class="lead">Cookies</p>
+            <div class="list-group">
+                <a href="lfive.php" class="list-group-item">Last five visited items</a>
+                <a href="mfive.php" class="list-group-item">Five most visited items</a>
             </div>
+            <p class="lead">Partners</p>
+            <div class="list-group">
+                <a href="http://hroboter.com/" class="list-group-item">HRoboter</a>
+                <a href="http://taipham.info/index.php" class="list-group-item">SoccerGearX</a>
+                <a href="http://match-all.com/matchall/" class="list-group-item">Auto Car</a>
+            </div>
+        </div>
 
-            <div class="col-md-9">
+        <div class="col-md-9">
+
+            <div class="row carousel-holder">
+
                 <div class="col-md-12">
-                    <h4>Last five visited items</h4>
+                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img class="slide-image" src="http://match-all.com/matchall/photo/car1.jpg" alt="http://placehold.it/800x300">
+                            </div>
+                            <div class="item">
+                                <img class="slide-image" src="http://taipham.info/image/adidas-toque-13-jersey.jpg" alt="http://placehold.it/800x300">
+                            </div>
+                            <div class="item">
+                                <img class="slide-image" src="http://match-all.com/matchall/photo/car2.jpg" alt="http://placehold.it/800x300">
+                            </div>
+                        </div>
+                        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                        </a>
+                    </div>
                 </div>
 
-                <div class="row">
-                    <?php
-                        var_dump($_COOKIE);
-                    ?>
-                    <p>Need to get last five visited items</p>
-                </div>
+            </div>
+
+
+
+
+            <div class="row">
+                <?php
+                //get the last 5 visited items ID.
+                $arr=array_reverse($_COOKIE);
+                $keyArr= [];
+                $i=0;
+                foreach($arr as $value){
+                    array_push($keyArr, $value);
+                    $i++;
+
+                    if($i==5) break;
+                }
+                foreach ($keyArr as $value){
+                    $temp = explode("_", $value);
+
+                    if($temp[0] == "knowasian"){
+
+                        $link = "http://" . $temp[0] . ".com/companywebsite/getProduct.php?id=" . $temp[1];
+                        $temp = file_get_contents($link);
+                        $row = json_decode($temp, true);
+                        $temp= "http://knowasian.com/companywebsite/Tradition.php?id=" . $row["ID"];
+                        echo "<div style='float:left;margin:20px;'><label>" . $row["TITLE"] . "</label><br />";
+                        echo "<a href=" . $temp . ">" . "<img src=" . $row["PICTURE"] . " style='width:340px;height:228px;'>" . "</a></div>";
+                        $a="<a href=" . $temp . ">" . "<img src=" . $row["PICTURE"] . " style='width:340px;height:228px;'>" . "</a></div>";
+
+                    }
+                }
+                ?>
 
             </div>
 
         </div>
 
     </div>
-    <!-- /.container -->
 
-    <div class="container">
+</div>
+<!-- /.container -->
 
-        <hr>
+<div class="container">
 
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Agora team 2017</p>
-                </div>
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; Agora Team 2017</p>
             </div>
-        </footer>
+        </div>
+    </footer>
 
-    </div>
-    <!-- /.container -->
+</div>
+<!-- /.container -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
 
 </body>
 
