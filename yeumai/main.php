46a619375afcefc055e60d0ef9200464e0b74dda
Tai Pham <kael136@gmail.com>
2017-05-16 20:40:43 -0700

    fix navbar for mobile

--- yeumai/main.php
+++ yeumai/main.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<!--
 <head>
 
     <meta charset="utf-8">
@@ -14,18 +15,15 @@
 
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="bootstrap/css/shop-homepage.css" rel="stylesheet">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+   
 
 </head>
+-->
+<?php include 'utils/header_nav.inc.php'; ?>
 
 <body>
 
-    <?php include 'header_nav.inc.php'; ?>
+    
     
 
     <!-- Page Content -->
@@ -62,10 +60,10 @@
     <!-- /.container -->
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="bootstrap/js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
 
 </body>
 
